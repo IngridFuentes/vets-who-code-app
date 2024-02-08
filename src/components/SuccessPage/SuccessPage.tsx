49d315a28b85cb65c9e8f3e063182b1b285aa1e4
Ingrid04 <ingridfmelgar@gmail.com>
2024-02-07 23:02:27 -0500

    contact-us message confirmation done

--- src/components/SuccessPage/SuccessPage.tsx
+++ src/components/SuccessPage/SuccessPage.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Router from 'next/router';
+import styled from "styled-components";
+
+
+const Container = styled.div`
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    padding: 20px;
+    border-radius: 8px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    max-width: 400px;
+    width: 100%;
+    text-align: center;
+`;
+
+const SuccessMessage = styled.p`
+    color: red;
+    font-size: 18px;
+`;
 
 const SuccessPage: React.FC = () => {
 
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+          Router.push('/');
+        }, 5000);
+    
+        return () => clearTimeout(timeout);
+      }, [Router]);
+
     return (
-        <div>
-            <h2> Thank you for your message!</h2>
-            <p>We have received your message and will get back to you shortly.</p>
-        </div>
+        <Container>
+            <SuccessMessage> 
+                Thank you for your message!
+                We have received your message and will get back to you shortly.
+            </SuccessMessage>
+        </Container>
     );
 };
 
